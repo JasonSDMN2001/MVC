ffd32af7f1d3c56138b0798e33020fec6eab3d14
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-01-11 19:27:21 +0200

    an exei conflict mixali gamiesai

---
+++ WebApplication1/Models/PartialClasses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Models.Metadata;
+
+namespace WebApplication1.Models
+{
+    [ModelMetadataType(typeof(ProfessorMetadata))]
+        public partial class Professor
+        {
+            [Display(Name = "Full Name")]
+            public string Fullname
+            {
+                get
+                {
+                    return Name + "," + Surname;
+                }
+            }
+        }
+    
+}
